3d4085e0846ccbfcabdc1cb4fb610e212a501498
MoWen <963774034@qq.com>
2020-07-23 17:11:33 +0800

    0723

--- src/main/java/life/modawen/community/controller/AuthorizeController.java
+++ src/main/java/life/modawen/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import life.modawen.community.dto.AccessTokenDTO;
 import life.modawen.community.dto.GithubUser;
 import life.modawen.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,16 +15,23 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientID;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     //调用callback拿到code、state
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
         //全部传回去
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("f801f75fe735d26449e2");
-        accessTokenDTO.setClient_secret("ce3473a9554cef45fcaac2d141d39b258985ac00");
+        accessTokenDTO.setClient_id(clientID);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         //拿到string
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
