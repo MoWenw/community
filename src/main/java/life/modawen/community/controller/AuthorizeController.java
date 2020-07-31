e11deade8eb057b9cc39635ed4435f74d0ea1242
MoWen <963774034@qq.com>
2020-07-31 16:49:37 +0800

    0731

--- src/main/java/life/modawen/community/controller/AuthorizeController.java
+++ src/main/java/life/modawen/community/controller/AuthorizeController.java
@@ -2,7 +2,9 @@ package life.modawen.community.controller;
 
 import life.modawen.community.dto.AccessTokenDTO;
 import life.modawen.community.dto.GithubUser;
+import life.modawen.community.mapper.UserMapper;
 import life.modawen.community.provider.GithubProvider;
+import life.modawen.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -23,6 +26,9 @@ public class AuthorizeController {
     private String clientSecret;
     @Value("${github.redirect.uri}")
     private String redirectUri;
+
+    @Autowired
+    private UserMapper userMapper;
 
     //调用callback拿到code、state
     @GetMapping("/callback")
@@ -38,11 +44,18 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         //拿到string
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if (user!=null){
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser!=null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
             //登录成功写cookie和session
             //把user对象放到session里面（账户创建成功）,通过写入session方式，记录当前登录态
-            request.getSession().setAttribute("user",user);
+            request.getSession().setAttribute("user",githubUser);
             //跳转到index页面，不写跳转，当前地址不变，只是把页面渲染成index
             return "redirect:/";
         }else{
--- src/main/java/life/modawen/community/mapper/UserMapper.java
+++ src/main/java/life/modawen/community/mapper/UserMapper.java
@@ -1,11 +1,13 @@
 package life.modawen.community.mapper;
 
+import life.modawen.community.model.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 @Mapper
-public class UserMapper {
-    @Select("SELECT * FROM CITY WHERE state = #{state}")
-    City findByState(@Param("state") String state);
+public interface UserMapper {
+    @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+
 
 }
