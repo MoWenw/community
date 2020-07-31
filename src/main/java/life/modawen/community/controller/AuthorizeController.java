68534e3cdf7b5459d5e929e9510f66aa5fc0c5d0
MoWen <963774034@qq.com>
2020-07-31 22:05:35 +0800

    0731

--- src/main/java/life/modawen/community/controller/AuthorizeController.java
+++ src/main/java/life/modawen/community/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -34,7 +36,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request){
+                           HttpServletRequest request,
+                           HttpServletResponse response){
         //全部传回去
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientID);
@@ -46,16 +49,22 @@ public class AuthorizeController {
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(accessToken);
         if (githubUser!=null){
+            //获取用户信息
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            //生成token，放到user对象里面
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
+            //插入到数据库中
             userMapper.insert(user);
+            //把token放到cookie里面
+            response.addCookie(new Cookie("token",token));
             //登录成功写cookie和session
             //把user对象放到session里面（账户创建成功）,通过写入session方式，记录当前登录态
-            request.getSession().setAttribute("user",githubUser);
+            //request.getSession().setAttribute("user",githubUser);
             //跳转到index页面，不写跳转，当前地址不变，只是把页面渲染成index
             return "redirect:/";
         }else{
--- src/main/java/life/modawen/community/controller/IndexController.java
+++ src/main/java/life/modawen/community/controller/IndexController.java
@@ -1,10 +1,38 @@
 package life.modawen.community.controller;
 
+import life.modawen.community.mapper.UserMapper;
+import life.modawen.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+    //注入之前写的usermapper
+    @Autowired
+    private UserMapper userMapper;
+
+    //访问首页
     @GetMapping("/")
-    public String index(){ return "index"; }
+    public String index(HttpServletRequest request){
+        //循环看所有的cookie
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie: cookies){
+            //找到cookie=token的cookie
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                //数据库里查是不是有这条cookie记录
+                User user = userMapper.findByToken(token);
+                //验证前端的情况
+                if(user !=null){
+                    //如果有，cookie放到session里面
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+        return "index"; }
 }
