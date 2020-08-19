41de898f76b01aeced1d1b56ae94d16f7663aca2
MoWen <963774034@qq.com>
2020-08-19 19:20:12 +0800

    0819

--- src/main/java/life/modawen/community/interceptor/WebConfig.java
+++ src/main/java/life/modawen/community/interceptor/WebConfig.java
@@ -2,10 +2,10 @@ package life.modawen.community.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
+//@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
