2bc560b7c52d178635de97ae5618ffba97234405
MoWen <963774034@qq.com>
2020-08-22 20:15:22 +0800

    comment-1

---
+++ src/main/java/life/modawen/community/controller/CommentController.java
@@ -0,0 +1,29 @@
+package life.modawen.community.controller;
+
+import life.modawen.community.dto.CommentDTO;
+import life.modawen.community.mapper.CommentMapper;
+import life.modawen.community.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class CommentController {
+
+    @Autowired
+    private CommentMapper commentMapper;
+
+    @ResponseBody
+    @RequestMapping(value = "/comment",method = RequestMethod.POST)
+    public Object post(@RequestBody CommentDTO commentDTO){
+        Comment comment = new Comment();
+        comment.setParentId(commentDTO.getParentId());
+        comment.setContent(commentDTO.getContent());
+        comment.setType(commentDTO.getType());
+        comment.setGmtModified(System.currentTimeMillis());
+        comment.setGmtCreate(System.currentTimeMillis());
+        comment.setCommentator(1);
+        commentMapper.insert(comment);
+        return null;
+    }
+}
