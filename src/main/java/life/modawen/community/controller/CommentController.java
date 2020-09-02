c68136b0070db572bb2fbd3f2461474b615a7ccb
MoWen <963774034@qq.com>
2020-09-02 11:49:51 +0800

    comment-5

--- src/main/java/life/modawen/community/controller/CommentController.java
+++ src/main/java/life/modawen/community/controller/CommentController.java
@@ -6,6 +6,7 @@ import life.modawen.community.exception.CustomizeErrorCode;
 import life.modawen.community.model.Comment;
 import life.modawen.community.model.User;
 import life.modawen.community.service.CommentService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -26,6 +27,11 @@ public class CommentController {
         if (user == null) {
             return ResultDTO.errorOf(CustomizeErrorCode.NO_LOGIN);
         }
+
+        if(commentCreateDTO == null || StringUtils.isBlank(commentCreateDTO.getContent())){
+            return ResultDTO.errorOf(CustomizeErrorCode.COMMENT_IS_EMPTY);
+        }
+
         Comment comment = new Comment();
         comment.setParentId(commentCreateDTO.getParentId());
         comment.setContent(commentCreateDTO.getContent());
