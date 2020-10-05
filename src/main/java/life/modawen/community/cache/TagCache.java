944447694f10e627b70297af1c32377b8983cb34
MoWen <963774034@qq.com>
2020-10-05 21:22:50 +0800

    text

--- src/main/java/life/modawen/community/cache/TagCache.java
+++ src/main/java/life/modawen/community/cache/TagCache.java
@@ -14,7 +14,7 @@ public class TagCache {
         List<TagDTO> tagDTOS = new ArrayList<>();
         TagDTO program = new TagDTO();
         program.setCategoryName("开发语言");
-        program.setTags(Arrays.asList("js","php","css","html","node","python","javascript","C++","golang","objective-c","ruby","C#"));
+        program.setTags(Arrays.asList("java","js","php","css","html","node","python","javascript","C++","golang","objective-c","ruby","C#"));
         tagDTOS.add(program);
 
         TagDTO framework = new TagDTO();
