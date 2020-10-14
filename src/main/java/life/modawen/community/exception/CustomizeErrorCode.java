d6e0d5756df348d07896879c643bc613151b0720
MoWen <963774034@qq.com>
2020-10-14 20:22:01 +0800

    picture-2

--- src/main/java/life/modawen/community/exception/CustomizeErrorCode.java
+++ src/main/java/life/modawen/community/exception/CustomizeErrorCode.java
@@ -10,6 +10,7 @@ public enum CustomizeErrorCode implements ICustomizeErrorCode {
     COMMENT_IS_EMPTY(2007, "输入内容不能为空"),
     READ_NOTIFICATION_FAIL(2008, "读到别人的信息啦！检查代码吧！"),
     NOTIFICATION_NOT_FOUND(2009, "消息不翼而飞了"),
+    FILE_UPLOAD_FAIL(2010, "图片上传失败"),
     ;
 
     @Override
