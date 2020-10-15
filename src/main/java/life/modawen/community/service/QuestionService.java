915fd74be435f80b0ca90b7a8d55dbc7ddd7131b
MoWen <963774034@qq.com>
2020-10-15 16:25:54 +0800

    search

--- src/main/java/life/modawen/community/service/QuestionService.java
+++ src/main/java/life/modawen/community/service/QuestionService.java
@@ -2,6 +2,7 @@ package life.modawen.community.service;
 
 import life.modawen.community.dto.PaginationDTO;
 import life.modawen.community.dto.QuestionDTO;
+import life.modawen.community.dto.QuestionQueryDTO;
 import life.modawen.community.exception.CustomizeErrorCode;
 import life.modawen.community.exception.CustomizeException;
 import life.modawen.community.mapper.QuestionExtMapper;
@@ -34,11 +35,19 @@ public class QuestionService {
     @Autowired
     private UserMapper userMapper;
 
-    public PaginationDTO list(Integer page, Integer size) {
+    public PaginationDTO list(String search, Integer page, Integer size) {
+        if(StringUtils.isNotBlank(search)){
+            String[] tags = StringUtils.split(search, " ");
+            search = Arrays.stream(tags).collect(Collectors.joining("|"));
+        }
 
         PaginationDTO paginationDTO = new PaginationDTO();
         Integer totalPage;
-        Integer totalCount = (int) questionMapper.countByExample(new QuestionExample());
+        QuestionQueryDTO questionQueryDTO = new QuestionQueryDTO();
+        questionQueryDTO.setSearch(search);
+
+        Integer totalCount = questionExtMapper.countBySearch(questionQueryDTO);
+
         if (totalCount % size == 0){
             totalPage = totalCount / size;
         } else{
@@ -52,11 +61,12 @@ public class QuestionService {
             page = totalPage;
         }
         paginationDTO.setPagination(totalPage, page);
-        //size*(page-1)
         Integer offset = size * (page - 1);
         QuestionExample questionExample = new QuestionExample();
         questionExample.setOrderByClause("gmt_create desc");
-        List<Question> questions=questionMapper.selectByExampleWithRowbounds(questionExample, new RowBounds(offset,size));
+        questionQueryDTO.setSize(size);
+        questionQueryDTO.setPage(offset);
+        List<Question> questions = questionExtMapper.selectBySearch(questionQueryDTO);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
 
         for(Question question:questions){
